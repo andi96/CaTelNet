6164d5f3df6cf5ed037bf962dc67918b2505ee61
andi96 <32385710+andi96@users.noreply.github.com>
2017-10-21 23:14:02 +0300

    Saptamana 4
    Javascript
    schimb header cand hover peste sigla
    jQuery 
    slider 
    lightbox 
    continut Acasa , Servicii si partial Produse

---
+++ script.js
@@ -0,0 +1,16 @@
+function SiglaonHover()
+{
+	
+	console.log("hello");
+    $("#header").css("background","#0080ff");
+	$("#header h1").css("color","#003366");
+	$("#header h2").css("color","#003366");
+}
+
+function SiglaoffHover()
+{
+	console.log("bye");
+    $("#header").css("background","#3399ff");
+	$("#header h1").css("color","black");
+	$("#header h2").css("color","black");
+}
