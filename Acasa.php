a62c3f5574188e5c9b41d2e6cd61321550b0954d
andi96 <32385710+andi96@users.noreply.github.com>
2017-10-29 08:14:44 +0200

    Acasa.html
    Saptamana 5

---
+++ Acasa.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+ 
+<title> CaTelNet </title>
+
+<!-- CSS si Javascript extern comun tuturor paginilor ////////////////////////////////////// -->
+<?php include 'external_css_javascript.php' ?>
+
+<!-- CSS pentru continutul ( id="content" ) paginii -->
+<style>
+
+</style>
+
+</head>
+
+
+<body>
+
+<!-- Header ////////////////////////////////////// -->
+<?php include 'header.php'; ?>
+
+<!-- Slider ////////////////////////////////////// -->
+<?php include 'slider.php'; ?>
+
+
+<div id="main">
+
+<!-- Meniu stanga ////////////////////////////////////// -->
+<?php include 'meniu_stanga_si_sus.php'; ?>
+
+
+<!-- Continut ////////////////////////// -->
+	
+	<div id="content">	
+		<h3 style="margin-left:20px"> Acasa </h3>
+		<pre style="font-family: 'Times New Roman', Times, serif;"> 
+    SC CaTelNet SRL prin serviciile de televiziune prin cablu, telefonie fixa si internet, oferite, vine in intampinarea nevoilor clientilor astfel 
+incat sa fim un partener de incredere in colaborarile viitoare.
+    Echipa pe care ne bazam, seriozitatea si relatiile cu partenerii de afaceri, reprezinta garantia ca serviciile sunt de cea mai buna calitate.
+    Colaborand cu noi veti avea satisfactie garantata.
+    Afacerile care merita facute sunt cele bune, cele foarte bune si cele impreuna cu noi.
+		
+    Procedura functionala de solutionare a reclamatiilor.
+	
+    SC CaTelNet SRL pune la dispozitia clientilor urmatoarele modalitati de preluare si solutionare a sesizarilor :
+    - telefonic 7/7 zile, 24/24 ore la nr. 0744 310 710
+    - la sediul central din str.Revolutiei, nr.1
+    - email : catelnet@tvcatelnet.ro
+		</pre>
+	</div>
+	
+<!-- End Continut ////////////////////////// -->
+	
+</div>
+
+
+<!-- Footer ////////////////////////////////////// -->
+<?php include 'footer.php'; ?>
+
+</body>
+
+
+</html>
